d15802d4edf8a58c376ffe7f23a554926027cfc2
maftoun95 <maftoun95@gmail.com>
2017-01-05 17:46:14 -0800

    more assignments: debugging, students, etc

--- WF/students and instructors/script.js
+++ WF/students and instructors/script.js
@@ -1,5 +1,5 @@
 //Part I
-var students = [ 
+var students = [
      {first_name:  'Michael', last_name : 'Jordan'},
      {first_name : 'John', last_name : 'Rosales'},
      {first_name : 'Mark', last_name : 'Guillen'},
@@ -7,19 +7,18 @@ var students = [
 ]
 
 function whoDat(arr) {
-	for ( var key in arr) {
-		console.log(key);
-		console.log(arr[key]);
+	for (var key in arr) {
+        var obj = arr[key];
+        console.log(`${obj.first_name} ${obj.last_name}`);
 	}
 }
 
 whoDat(students);
 
-
 //Part II
 
 var users = {
- 'Students': [ 
+ 'Students': [
      {first_name:  'Michael', last_name : 'Jordan'},
      {first_name : 'John', last_name : 'Rosales'},
      {first_name : 'Mark', last_name : 'Guillen'},
@@ -30,3 +29,20 @@ var users = {
      {first_name : 'Martin', last_name : 'Puryear'}
   ]
  }
+
+ function whoDem(arr) {
+    for (var key in arr){
+        console.log(key);
+        var userArray = users[key]
+        for(var o in userArray) {
+            var num = o;
+            num++;
+            var object = userArray[o];
+            var strlen = (object.first_name.length)+(object.last_name.length);
+
+            console.log(`${num} - ${object.first_name} ${object.last_name} - ${strlen}`);
+        }
+    }
+ }
+
+ whoDem(users);
