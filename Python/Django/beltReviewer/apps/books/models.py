39d68b191c117ef9d3a1df1450c2b290181581f4
Saman M <maftoun95@gmail.com>
2017-02-03 07:59:31 -0800

    pre belt push

--- Python/Django/beltReviewer/apps/books/models.py
+++ Python/Django/beltReviewer/apps/books/models.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-
+from ..loginreg.models import Users
 from django.db import models
 
 class BookManager(models.Manager):
@@ -9,9 +9,17 @@ class BookManager(models.Manager):
 
 class Books(models.Model):
     name = models.CharField(max_length=255)
-    review = models.TextField()
-    rating = models.IntegerField()
+    author = models.CharField(max_length=255)
     created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
 
     objects = BookManager()
+
+class ReviewManager(models.Manager):
+    pass
+
+class Reviews(models.Model):
+    author = models.ForeignKey(Users)
+    book = models.ForeignKey(Books)
+    rating = models.IntegerField()
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
