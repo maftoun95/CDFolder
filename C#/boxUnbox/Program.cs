d8e5e31b20a5c2b48d05e91d69d1d1497fae0f82
Saman M <maftoun95@gmail.com>
2017-02-07 14:43:12 -0800

    box unbox

--- C#/boxUnbox/Program.cs
+++ C#/boxUnbox/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
@@ -6,7 +7,22 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            
+            List<object> ting = new List<object>();
+            ting.Add(7);
+            ting.Add(28);
+            ting.Add(-1);
+            ting.Add(true);
+            ting.Add("chair");
+            int sum = 0;
+            for (int i = 0; i < ting.Count; i++) 
+            {
+                if (ting[i] is int)
+                {
+                    sum += (int)ting[i];
+                }
+                System.Console.WriteLine(ting[i]);
+            }
+            System.Console.WriteLine(sum);
         }
     }
 }
