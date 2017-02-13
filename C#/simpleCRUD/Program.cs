4060cec45b6b1fe481c9c1b692c715ff1242b329
Saman M <maftoun95@gmail.com>
2017-02-13 06:23:50 -0800

    moduarizing simpleCRUD

--- C#/simpleCRUD/Program.cs
+++ C#/simpleCRUD/Program.cs
@@ -6,6 +6,14 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            // write();
+            // read();
+            //update();
+            delete();
+
+        }
+
+        public static void write(){
             System.Console.WriteLine("enter your first name: ");
             string firstName = System.Console.ReadLine();
             System.Console.WriteLine("enter your last name: ");
@@ -14,9 +22,38 @@ namespace ConsoleApplication
             string favoriteNumber = System.Console.ReadLine();
             
 
-            string query = $"INSERT INTO users (firstName,lastName,favoriteNumber) VALUES (" + '"' + firstName + '"' + "," + '"' + lastName + '"' + "," + '"' + favoriteNumber + '"' + ");";
+            string query = "INSERT INTO users (firstName,lastName,favoriteNumber) VALUES (" + '"' + firstName + '"' + "," + '"' + lastName + '"' + "," + '"' + favoriteNumber + '"' + ");";
             
             System.Console.WriteLine("the query to be run:   "+query);
+        }
+        public static void read(){
+            System.Console.WriteLine("Hey broski. Would you like to read the whole database (enter 1) or read just one column (enter 2)");
+            string selection = System.Console.ReadLine();
+            if (selection == "1"){
+                string query = "SELECT * FROM users";
+                System.Console.WriteLine("the query to be run:   "+query);
+            } else if (selection == "2"){
+                System.Console.WriteLine("Sure, who would you like to know about? Type their first name:");
+                string fname = System.Console.ReadLine();
+
+                string query = "SELECT * FROM users WHERE (firstName=" + '"' + fname + '"' + ")";
+                System.Console.WriteLine("the query to be run:   "+query);
+            } else {
+                System.Console.WriteLine("Not a valid response, lets try this again....");
+                read();
+            }
+        }
+        public static void update(){
+            System.Console.WriteLine("Sounds good, Who would you like to update?? Enter a first name:");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Sure thing, and what column name would you like to update?");
+            System.Console.WriteLine("Enter either either firstname, lastName, or favoriteNumber.");
+            //the following query stmt needs work
+            string query = "UPDATE users SET (firstName=" + '"' + "INPUTvARIABLE" + '"' + ")";
+        }
+        public static void delete(){
+            System.Console.WriteLine("O sad.");
+            System.Console.WriteLine("So who\'s getting nixed?");
         }
     }
 }
